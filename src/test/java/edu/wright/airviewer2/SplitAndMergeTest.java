2b9fd5f8e0078d84201e32e25d1380766a00372a
paidikondala <paidikondala.3@wright.edu>
2022-04-29 13:48:57 -0400

    Issue #30 - Fixed review comments

--- src/test/java/edu/wright/airviewer2/SplitAndMergeTest.java
+++ src/test/java/edu/wright/airviewer2/SplitAndMergeTest.java
@@ -2,7 +2,6 @@ package edu.wright.airviewer2;
 
 import static org.junit.Assert.*;
 
-
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
@@ -31,93 +30,90 @@ import javafx.scene.image.ImageView;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 
-
-
 public class SplitAndMergeTest extends ApplicationTest {
-	
-	
-	@Test
-	public void mergefileTest()  {
-		try {
-		Pagination pagination=  new Pagination();
-		Path currentDir = Paths.get(".");
-		String current = currentDir.toAbsolutePath() + "/src/test/resources/test.pdf";
-		Path exist = Paths.get(current);
-		AIRViewerModel model = new AIRViewerModel(exist);
-		pagination.setPageCount(model.numPages());
-	    pagination.setDisable(true);
-	    Group pageImageGroup = new Group();
-	    pagination.setPageFactory(index -> {
-	    	System.out.println("haiinpagena");
-            ImageView currentPageImageView = new ImageView(model.getImage(0));
-            pageImageGroup.getChildren().clear();
-            pageImageGroup.getChildren().add(currentPageImageView);
-   
-			return pageImageGroup;
-        });
-        File f = new File(current);
-		//when(fileChooser.showOpenDialog(any()).thenReturn(fileChooser);
-		Platform.runLater(new Runnable(){
+	/**
+	  * [issue] (https://github.com/WSU-CEG-6110-4410/AirViewer/issues/30)
+	  * These are the test cases for merge functionality
+	  */
 
-			@Override
-			public void run() {
-				// TODO Auto-generated method stub
-				try {
-					new SplitAndMerge().mergefile(pagination,model);
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-				
-			}
-			// do your GUI stuff here
-			});
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
 	@Test
-	public void splitterTest() {
+	public void mergefileTest() {
 		try {
-			Pagination pagination=  new Pagination();
+			Pagination pagination = new Pagination();
 			Path currentDir = Paths.get(".");
 			String current = currentDir.toAbsolutePath() + "/src/test/resources/test.pdf";
 			Path exist = Paths.get(current);
 			AIRViewerModel model = new AIRViewerModel(exist);
 			pagination.setPageCount(model.numPages());
-		    pagination.setDisable(true);
-		    Group pageImageGroup = new Group();
-		    pagination.setPageFactory(index -> {
-		    	System.out.println("haiinpagena");
-	            ImageView currentPageImageView = new ImageView(model.getImage(0));
-	            pageImageGroup.getChildren().clear();
-	            pageImageGroup.getChildren().add(currentPageImageView);
-	   
-				return pageImageGroup;
-	        });
-	        File f = new File(current);
-			//when(fileChooser.showOpenDialog(any()).thenReturn(fileChooser);
-			Platform.runLater(new Runnable(){
+			pagination.setDisable(true);
+			Group pageImageGroup = new Group();
+			pagination.setPageFactory(index -> {
+			ImageView currentPageImageView = new ImageView(model.getImage(0));
+			pageImageGroup.getChildren().clear();
+			pageImageGroup.getChildren().add(currentPageImageView);
+
+			return pageImageGroup;
+			});
+			File f = new File(current);
+			Platform.runLater(new Runnable() {
 
 				@Override
 				public void run() {
 					// TODO Auto-generated method stub
 					try {
-						new SplitAndMerge().splitter("1",pagination,model);
+						new SplitAndMerge().mergefile(pagination, model);
 					} catch (Exception e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-					
-					
 				}
-				// do your GUI stuff here
-				});
-			} catch(Exception e) {
-				e.printStackTrace();
-			}
+			});
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	  * [issue] (https://github.com/WSU-CEG-6110-4410/AirViewer/issues/29)
+	  * These are the test cases for split functionality
+	  */
+
+	@Test
+	public void splitterTest() {
+		try {
+			Pagination pagination = new Pagination();
+			Path currentDir = Paths.get(".");
+			String current = currentDir.toAbsolutePath() + "/src/test/resources/test.pdf";
+			Path exist = Paths.get(current);
+			AIRViewerModel model = new AIRViewerModel(exist);
+			pagination.setPageCount(model.numPages());
+			pagination.setDisable(true);
+			Group pageImageGroup = new Group();
+			pagination.setPageFactory(index -> {
+			ImageView currentPageImageView = new ImageView(model.getImage(0));
+			pageImageGroup.getChildren().clear();
+			pageImageGroup.getChildren().add(currentPageImageView);
+
+			return pageImageGroup;
+			});
+			File f = new File(current);
+			Platform.runLater(new Runnable() {
+
+				@Override
+				public void run() {
+					// TODO Auto-generated method stub
+					try {
+						new SplitAndMerge().splitter("1", pagination, model);
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
+				}
+			});
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
